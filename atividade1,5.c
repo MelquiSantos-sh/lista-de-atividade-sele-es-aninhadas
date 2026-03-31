6fbda8e312f44207182eaff53fc80bc1a854435c
Melquizedequedos <melquizedequesantos13@gmail.com>
2026-03-31 19:17:24 -0300

    final

---
+++ atividade1,5.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main(){
+
+    char drama, aventura, suspense, fantasia;
+    char comedia, romance, musical, nerd;
+    char acao, policial, crime;
+
+    printf("Drama [S/N]? ");
+    scanf(" %c", &drama);
+
+    if (drama == 'S' || drama == 's') {
+        
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
